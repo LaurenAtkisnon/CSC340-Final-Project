f242678ec3a6909101288eae766caa52e6071411
tjcj211 <timcarta@hotmail.com>
2021-05-03 20:18:21 -0400

    Game waits for 4 Players before starting. Needs to have countdown implemented into chat box.
    Co-authored-by: Ryan Hayes <rhayes923@users.noreply.github.com>

--- GameEngine.java
+++ GameEngine.java
@@ -1,9 +1,10 @@
+
 /***************
  * GameEngine
  * Author: Christian Duncan
  * Spring 21: CSC340
  * 
- * Modified By: Charles Rescsanski
+ * Modified By: Charles Rescsanski, Timothy Carta, Ryan Hayes
  * 
  * This is the Engine for the TRON game.  It is NOT a standalone application.  It relays commands
  * to the GameState.
@@ -22,19 +23,20 @@ public class GameEngine implements Runnable {
     Debug debug;
     boolean activeGame;
     boolean done;
+    boolean keepGoing;
+    public static final int NUM_PLAYERS_BEFORE_START = 4;
 
-    public GameEngine()
-    {
-    	this.activeGame = true;
+    public GameEngine() {
+        this.activeGame = false;
         gameState = new GameState();
         debug = Debug.getInstance();
+        keepGoing = true;
     }
 
     /**
-
-    /**
-     * Return a (deep) clone of the game state.
-     * Thus any changes to the game state must be made through the game server.
+     * 
+     * /** Return a (deep) clone of the game state. Thus any changes to the game
+     * state must be made through the game server.
      **/
     public synchronized GameState getGameState() {
         try {
@@ -42,76 +44,74 @@ public class GameEngine implements Runnable {
             ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(byteArray));
             GameState result = (GameState) in.readObject();
             return result;
-        } catch (IOException|ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException e) {
             debug.println(1, "Error: gameEngine.gGSB - converting game state to byte array.");
         }
         return null;
     }
 
     public synchronized byte[] getGameStateBytes() {
-        try{
+        try {
             ByteArrayOutputStream bout = new ByteArrayOutputStream();
-        ObjectOutputStream out = new ObjectOutputStream(bout);
-        synchronized (gameState) {
-            out.writeObject(gameState);
-        }
-        out.close();
-        bout.close();
-        return bout.toByteArray();
+            ObjectOutputStream out = new ObjectOutputStream(bout);
+            synchronized (gameState) {
+                out.writeObject(gameState);
+            }
+            out.close();
+            bout.close();
+            return bout.toByteArray();
         } catch (IOException e) {
             debug.println(1, "Error: gameEngine.gGSB - converting game state to byte array.");
             return null;
         }
-        
+
     }
-    
-    public void turnWest(int p){
+
+    public void turnWest(int p) {
         gameState.turnWest(p);
     }
 
-    public void turnEast(int p){
-       gameState.turnEast(p);
+    public void turnEast(int p) {
+        gameState.turnEast(p);
     }
 
-    public void turnSouth(int p){
-    	gameState.turnSouth(p);
+    public void turnSouth(int p) {
+        gameState.turnSouth(p);
     }
 
-    public void turnNorth(int p){
+    public void turnNorth(int p) {
         gameState.turnNorth(p);
-     }
-
-    public synchronized void changePlayerDirection(int p, int newDirection)
-    {
-    	switch (newDirection)
-    	{
-    		case 0: 
-    			this.turnNorth(p);
-    			break;
-    		case 1:
-    			this.turnEast(p);
-    			break;
-    		case 2:
-    			this.turnSouth(p);
-    			break;
-    		case 3:
-    			this.turnWest(p);
-    			break;
-    			
-    	}
     }
-    
+
+    public synchronized void changePlayerDirection(int p, int newDirection) {
+        switch (newDirection) {
+            case 0:
+                this.turnNorth(p);
+                break;
+            case 1:
+                this.turnEast(p);
+                break;
+            case 2:
+                this.turnSouth(p);
+                break;
+            case 3:
+                this.turnWest(p);
+                break;
+
+        }
+    }
 
     public synchronized int addPlayer(String name, Color color) {
-    	Random rand = new Random();
-    	int initialDirection = rand.nextInt(4);
+        Random rand = new Random();
+        int initialDirection = rand.nextInt(4);
         return gameState.addPlayer(name, color, initialDirection);
     }
 
     /**
-     * Set a player p's direction to dx and dy.
-     * This moves all cells in that direction
-     * @param p The player (index) to move
+     * Set a player p's direction to dx and dy. This moves all cells in that
+     * direction
+     * 
+     * @param p  The player (index) to move
      * @param dx The amount to move in the x direction
      * @param dy The amount to move in the y direction
      **/
@@ -119,32 +119,41 @@ public class GameEngine implements Runnable {
         gameState.setPlayerLocation(p, dx, dy);
     }
 
+    public synchronized void startGame() {
+        activeGame = true;
+    }
+
     public void run() {
         while (!gameState.isDone()) {
             debug.println(10, "(GameEngine.run) Executing...");
-            
-            if (this.activeGame)
-            {
-            	//Move Players
+
+            // If enough players to start
+            if (gameState.getPlayers().size() >= NUM_PLAYERS_BEFORE_START && keepGoing) {
+                startGame();
+            }
+
+            if (this.activeGame) {
+                // Move Players
                 movePlayers();
-                
-                //Check if game is over
-                if(checkIfGameOver())
-            	{
-                	this.activeGame = false;
-                	//gameState.endCurrentGame();
-            	}
-            }       
+
+                // Check if game is over
+                if (checkIfGameOver()) {
+                    this.activeGame = false;
+                    keepGoing = false;
+                    // gameState.endCurrentGame();
+                }
+            }
 
             try {
                 Thread.sleep(GameServer.GAME_STATE_REFRESH);
-            } catch (Exception e) { }
-        } 
+            } catch (Exception e) {
+            }
+        }
     }
 
     private synchronized void detectCollisions() {
         ArrayList<GameState.Player> player = gameState.getPlayers();
-        
+
         // Each player cannot cross any line (including its own).
         // For each player, we need to check whether their current location intersects
         // with a previously visited location or the wall of the arena.
@@ -152,54 +161,46 @@ public class GameEngine implements Runnable {
         int size = player.size();
         for (int i = 0; i < size; i++) {
             GameState.Player p = player.get(i);
-            p.collisions();  // Compute collisions for this player
+            p.collisions(); // Compute collisions for this player
 
         }
     }
-    
+
     private synchronized void movePlayers() {
-    	ArrayList<GameState.Player> player = gameState.getPlayers();
-    	int size = player.size();
+        ArrayList<GameState.Player> player = gameState.getPlayers();
+        int size = player.size();
         for (int i = 0; i < size; i++) {
             GameState.Player p = player.get(i);
             p.move();
 
-        }        
-        
-        //next, we need to update the grid to reflect the new position of each player
+        }
+
+        // next, we need to update the grid to reflect the new position of each player
         gameState.updateGrid();
-    	
+
     }
-    
+
     private synchronized boolean checkIfGameOver() {
-    	ArrayList<GameState.Player> player = gameState.getPlayers();
-    	int size = player.size();
-    	if (size > 0)
-    	{
-    		int alive = 0;
+        ArrayList<GameState.Player> player = gameState.getPlayers();
+        int size = player.size();
+        if (size > 0) {
+            int alive = 0;
             for (int i = 0; i < size; i++) {
                 GameState.Player p = player.get(i);
-                if (!p.dead)
-                {
-                	alive++;
+                if (!p.dead) {
+                    alive++;
                 }
-            }   
-            //a multiplayer game ends when there is only one player left standing
-            if (size > 1)
-            {
-            	return alive == 1;	
             }
-            else
-            {
-            	//in single player mode, the game simply ends when the only player dies
-            	return alive == 0;
+            // a multiplayer game ends when there is only one player left standing
+            if (size > 1) {
+                return alive == 1;
+            } else {
+                // in single player mode, the game simply ends when the only player dies
+                return alive == 0;
             }
-    	}
-    	
-    	return false;
-    	
-        
-       	
+        }
+
+        return false;
+
     }
 }
-
--- GameState.java
+++ GameState.java
@@ -1,3 +1,4 @@
+
 /***********
  * Game State
  * Author: Christian Duncan
@@ -18,26 +19,26 @@ import java.awt.geom.Point2D;
 
 public class GameState implements Cloneable, Serializable {
     public static final long serialVersionUID = 340L;
-    public static final int GRID_HEIGHT = 100; //height
-    public static final int GRID_WIDTH = 100; //width
-    
-    // Inner class: Just a player, with name, color, location, direction, and 
-    class Player implements Cloneable, Serializable {
-    	
-        public static final int DIRECTION_NORTH = 0; //north
-        public static final int DIRECTION_EAST = 1; //EAST
-        public static final int DIRECTION_SOUTH = 2; //SOUTH
-        public static final int DIRECTION_WEST = 3; //WEST
+    public static final int GRID_HEIGHT = 100; // height
+    public static final int GRID_WIDTH = 100; // width
 
-    	boolean dead = false;
-        String name;  // Name to display
-        Color appearance;  // The appearance of this player and its corresponding lines
-        int gridID; //the value of grid coordinates pertaining to this player
-        int direction; //direction that player is moving
-        int locx; //current x coordinate of player
-        int locy; //current y coordinate of player
-        
-        public Player(String n, int _gridID, int initX,  int initY, Color _appearance, int _initialDirection) {
+    // Inner class: Just a player, with name, color, location, direction, and
+    class Player implements Cloneable, Serializable {
+
+        public static final int DIRECTION_NORTH = 0; // north
+        public static final int DIRECTION_EAST = 1; // EAST
+        public static final int DIRECTION_SOUTH = 2; // SOUTH
+        public static final int DIRECTION_WEST = 3; // WEST
+
+        boolean dead = false;
+        String name; // Name to display
+        Color appearance; // The appearance of this player and its corresponding lines
+        int gridID; // the value of grid coordinates pertaining to this player
+        int direction; // direction that player is moving
+        int locx; // current x coordinate of player
+        int locy; // current y coordinate of player
+
+        public Player(String n, int _gridID, int initX, int initY, Color _appearance, int _initialDirection) {
             this.name = n;
             this.locx = initX;
             this.locy = initY;
@@ -48,13 +49,21 @@ public class GameState implements Cloneable, Serializable {
 
         public Object clone() throws CloneNotSupportedException {
             Player p = (Player) super.clone();
-           
+
             return p;
         }
 
-        public String getName() { return this.name; }
-        public int getGridID() { return this.gridID; }
-        public Color getAppearance() { return this.appearance; }
+        public String getName() {
+            return this.name;
+        }
+
+        public int getGridID() {
+            return this.gridID;
+        }
+
+        public Color getAppearance() {
+            return this.appearance;
+        }
 
         /**
          * Moves player by corresponding distance
@@ -62,75 +71,78 @@ public class GameState implements Cloneable, Serializable {
         public void setLocation(int x, int y) {
             this.locx = x;
             this.locy = y;
-        }    
-        
+        }
+
         /**
          * Moves players by the given direction
          */
-        public void move()
-        {
-        	 switch(direction) {
+        public void move() {
+            switch (direction) {
 
-             case DIRECTION_NORTH:
-                 if (boundCheck(this.locx, this.locy-1)) {
-                     this.locy --;
-                 }
-                 break;
+                case DIRECTION_NORTH:
+                    if (boundCheck(this.locx, this.locy - 1)) {
+                        this.locy--;
+                    }
+                    break;
 
-             case DIRECTION_EAST:
-                 if (boundCheck(this.locx +1, this.locy)) {
-                     this.locx ++;
-                 } break;
+                case DIRECTION_EAST:
+                    if (boundCheck(this.locx + 1, this.locy)) {
+                        this.locx++;
+                    }
+                    break;
 
-             case DIRECTION_SOUTH:
-                 if (boundCheck(this.locx, this.locy+1)) {
-                     this.locy++;
-                 } break;
+                case DIRECTION_SOUTH:
+                    if (boundCheck(this.locx, this.locy + 1)) {
+                        this.locy++;
+                    }
+                    break;
 
-             case DIRECTION_WEST:
-                 if (boundCheck(this.locx-1, this.locy)) {
-                     this.locx --;
-                 } break;
-             }
-        	 
-        	 if(!lineCollisionCheck(this.locx, this.locy))
-        	 {
-        		 this.dead = true;
-        	 }
+                case DIRECTION_WEST:
+                    if (boundCheck(this.locx - 1, this.locy)) {
+                        this.locx--;
+                    }
+                    break;
+            }
+
+            if (!lineCollisionCheck(this.locx, this.locy)) {
+                this.dead = true;
+            }
         }
-        
-        public void turnWest(){
-            if(direction!= DIRECTION_EAST){
+
+        public void turnWest() {
+            if (direction != DIRECTION_EAST) {
                 direction = DIRECTION_WEST;
-            } }
+            }
+        }
 
-        public void turnEast(){
-            if(direction != DIRECTION_WEST){
+        public void turnEast() {
+            if (direction != DIRECTION_WEST) {
                 direction = DIRECTION_EAST;
-            } }
+            }
+        }
 
-        public void turnSouth(){
-            if(direction != DIRECTION_NORTH){
+        public void turnSouth() {
+            if (direction != DIRECTION_NORTH) {
                 direction = DIRECTION_SOUTH;
-            } }
+            }
+        }
 
-        public void turnNorth(){
-            if(direction != DIRECTION_SOUTH){
+        public void turnNorth() {
+            if (direction != DIRECTION_SOUTH) {
                 direction = DIRECTION_NORTH;
-            } }
-        
-   
-        
-         /**
+            }
+        }
+
+        /**
          * Determine any collisions between two groups of Players
          **/
         public void collisions() {
-            
+
         }
-        
+
         /**
-         * Generate a string representation of the given player
-         * For DEBUGGING purposes mainly
+         * Generate a string representation of the given player For DEBUGGING purposes
+         * mainly
          **/
         @Override
         public String toString() {
@@ -141,27 +153,27 @@ public class GameState implements Cloneable, Serializable {
     }
 
     // The list of Players
-    private ArrayList<Player> player;
+    private ArrayList<Player> players;
 
-    private int[][] grid = new int[GRID_WIDTH][GRID_HEIGHT]; //keeps track of where each player has been
-    
+    private int[][] grid = new int[GRID_WIDTH][GRID_HEIGHT]; // keeps track of where each player has been
+
     public GameState() {
-        player = new ArrayList<Player>(2);  // Initial size
-      
-         //set everything to 0
-         for (int x = 0; x < GRID_WIDTH; x++) {
+        players = new ArrayList<Player>();
+
+        // set everything to 0
+        for (int x = 0; x < GRID_WIDTH; x++) {
             for (int y = 0; y < GRID_HEIGHT; y++) {
                 this.grid[x][y] = 0;
-            } 
+            }
         }
-        
+
     }
 
     public Object clone() throws CloneNotSupportedException {
         GameState gs = (GameState) super.clone();
-        gs.player = new ArrayList<>(player.size());
-        for (int i = 0; i < player.size(); i++) {
-            gs.player.add((Player) player.get(i).clone());
+        gs.players = new ArrayList<>(players.size());
+        for (int i = 0; i < players.size(); i++) {
+            gs.players.add((Player) players.get(i).clone());
         }
         return gs;
     }
@@ -170,98 +182,99 @@ public class GameState implements Cloneable, Serializable {
      * Returns if the game is done or not!
      **/
     public boolean isDone() {
-        return false;  // For now, runs forever!
+        return false; // For now, runs forever!
     }
-    
-    
+
     /**
-     * Add a player to the Game State.  All future references to this 
-     * player should use the index returned.
-     * @param name The name of the player
+     * Add a player to the Game State. All future references to this player should
+     * use the index returned.
+     * 
+     * @param name  The name of the player
      * @param color The color of the player
      * @returns The index of this player (in the ArrayList)
      **/
     public int addPlayer(String name, Color color, int initialDirection) {
         // Pick an initial location that has not yet been visited
         Point p = starterPos();
-        int gridID = player.size() + 1;
-        player.add(new Player(name, gridID, (int)p.getX(), (int)p.getY(), color, initialDirection));
-        return player.size()-1;
+        int gridID = players.size() + 1;
+        players.add(new Player(name, gridID, (int) p.getX(), (int) p.getY(), color, initialDirection));
+        return players.size() - 1;
     }
 
-    /**Determines a location on the grid that has yet to be occupied */
-    public Point starterPos()
-    {
-        for (int x = GRID_WIDTH / 2; x < GRID_WIDTH; x++) {
-            for (int y = GRID_HEIGHT / 2; y < GRID_HEIGHT; y++) {
-              if (grid[x][y] == 0){
-                  return new Point(x, y);
-              }
+    /** Determines a location on the grid that has yet to be occupied */
+    public Point starterPos() {
+        boolean validStart = false;
+        int x = 0;
+        int y = 0;
+        while (!validStart) {
+            x = (int) (Math.random() * GRID_WIDTH);
+            y = (int) (Math.random() * GRID_HEIGHT);
+            if (x < GRID_WIDTH - 10 && x > 10 && y < GRID_HEIGHT - 10 && y > 10) {
+                validStart = true;
             }
         }
-        return null;
+        return new Point(x, y);
     }
 
     /**
-     * Set a player p's direction to dx and dy.
-     * This moves all cells in that direction
-     * @param p The player (index) to move
+     * Set a player p's direction to dx and dy. This moves all cells in that
+     * direction
+     * 
+     * @param p  The player (index) to move
      * @param dx The amount to move in the x direction
      * @param dy The amount to move in the y direction
      **/
     public void setPlayerLocation(int p, int x, int y) {
-        Player pl = player.get(p);  // Get the Player object
+        Player pl = players.get(p); // Get the Player object
         pl.setLocation(x, y);
-        //marks new location on grid
+        // marks new location on grid
         this.grid[x][y] = pl.getGridID();
     }
-    
-    public void turnWest(int p){
-    	Player pl = player.get(p);  // Get the Player object
-    	 pl.turnWest();
+
+    public void turnWest(int p) {
+        Player pl = players.get(p); // Get the Player object
+        pl.turnWest();
     }
 
-    public void turnEast(int p){
-    	Player pl = player.get(p);  // Get the Player object
-    	pl.turnEast();
+    public void turnEast(int p) {
+        Player pl = players.get(p); // Get the Player object
+        pl.turnEast();
     }
 
-    public void turnSouth(int p){
-    	Player pl = player.get(p);  // Get the Player object
-    	pl.turnSouth();
+    public void turnSouth(int p) {
+        Player pl = players.get(p); // Get the Player object
+        pl.turnSouth();
     }
 
-    public void turnNorth(int p){
-    	Player pl = player.get(p);  // Get the Player object
-    	pl.turnNorth();
-     }
-    
-    public void updateGrid()
-    {
-    	for (GameState.Player p : this.getPlayers())
-    	{
-    		if (!p.dead)
-    		{
-    			this.grid[p.locx][p.locy] = p.gridID;
-    		}
-    		
-    	}
+    public void turnNorth(int p) {
+        Player pl = players.get(p); // Get the Player object
+        pl.turnNorth();
     }
-    
-    //verify if location is valid
+
+    public void updateGrid() {
+        for (GameState.Player p : this.getPlayers()) {
+            if (!p.dead) {
+                this.grid[p.locx][p.locy] = p.gridID;
+            }
+
+        }
+    }
+
+    // verify if location is valid
     public boolean boundCheck(int x, int y) {
-    	return x > 0 && x < this.grid.length && y > 0 && y < this.grid[0].length;
-    }
-    
-    public boolean lineCollisionCheck(int x, int y)
-    {
-    	return this.grid[x][y] == 0;
+        return x > 0 && x < this.grid.length && y > 0 && y < this.grid[0].length;
     }
 
-    // Returns the list of players.  Probably safer to have some way to iterate through them and the cells
-    // So we can control access.  But this will be a network so this actually will be a local copy of the GameState anyway!
+    public boolean lineCollisionCheck(int x, int y) {
+        return this.grid[x][y] == 0;
+    }
+
+    // Returns the list of players. Probably safer to have some way to iterate
+    // through them and the cells
+    // So we can control access. But this will be a network so this actually will be
+    // a local copy of the GameState anyway!
     public ArrayList<Player> getPlayers() {
-        return player;
+        return players;
     }
 
     // Returns the grid containing the history of where each player has been
@@ -269,13 +282,12 @@ public class GameState implements Cloneable, Serializable {
         return grid;
     }
 
-    
     /**
      * Display the "Game State"
      **/
     public void display(PrintStream out) {
         out.println("============ State =================");
-        for (Player p: player) {
+        for (Player p : players) {
             out.println("  " + p);
         }
         out.println("====================================");
