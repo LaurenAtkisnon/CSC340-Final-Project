0b460319a22bac351635610e9cc7b1c201dc2411
crescsanski <54546006+crescsanski@users.noreply.github.com>
2021-05-04 07:11:27 -0400

    Await players before start (#3)
    * Game waits for 4 Players before starting.
    Needs to have countdown implemented into chat box.
    
    Co-authored-by: Ryan Hayes <rhayes923@users.noreply.github.com>
    
    * Added ghost players.
    Players that join mid game will be ghost players.
    They will see the board but not interact.
    
    * Game resets 10 seconds after gameover.
    
    Co-authored-by: tjcj211 <timcarta@hotmail.com>
    Co-authored-by: Ryan Hayes <rhayes923@users.noreply.github.com>
    Co-authored-by: Timothy Carta <47871807+tjcj211@users.noreply.github.com>

--- GameServer.java
+++ GameServer.java
@@ -1,9 +1,10 @@
+
 /***************
  * GameServer
  * Author: Christian Duncan
  * Spring 21: CSC340
  * 
- * Modified by: Charles Rescsanski
+ * Modified by: Charles Rescsanski, Timothy Carta, Ryan Hayes
  * 
  * This is a stand-alone application with the following functions:
  *  1. Listen for connections on a port.
@@ -18,33 +19,36 @@ import java.net.Socket;
 import java.util.HashSet;
 import java.io.*;
 
-
 public class GameServer implements Runnable {
     GameState gameState;
     GameEngine gameEngine;
-    public static final int GAME_STATE_REFRESH=75;
+    public static final int GAME_STATE_REFRESH = 100; // Set to 500 for slower speeds, good for debugging
     Debug debug;
     HashSet<Connection> connection; // The set of client connections
 
     public static final int DEFAULT_PORT = 1340;
 
-      // A new thread class to handle connections with a client
-      class Connection extends Thread {
+    // A new thread class to handle connections with a client
+    class Connection extends Thread {
         Socket socket;
         ObjectOutputStream out;
         ObjectInputStream in;
         boolean done;
-        String name;   // Name of the player 
-        Color color; //Color of player
-        int playerID; //the ID of the player;
+        String name; // Name of the player
+        Color color; // Color of player
+        int playerID; // the ID of the player;
 
         /**
          * The constructor
+         * 
          * @param socket The socket attached to this connection
-         * @param name The "name" of this connection - for debugging purposes
+         * @param name   The "name" of this connection - for debugging purposes
          **/
-        public Connection(Socket socket, String name) { 
-            this.socket = socket; done = false; this.name = name; this.playerID = -1;
+        public Connection(Socket socket, String name) {
+            this.socket = socket;
+            done = false;
+            this.name = name;
+            this.playerID = -1;
         }
 
         /**
@@ -53,8 +57,8 @@ public class GameServer implements Runnable {
         public void run() {
             try {
                 // First get the I/O streams for communication.
-                //   in -- Used to read input from client
-                //   out -- Used to send output to client
+                // in -- Used to read input from client
+                // out -- Used to send output to client
                 out = new ObjectOutputStream(socket.getOutputStream());
                 in = new ObjectInputStream(socket.getInputStream());
 
@@ -66,118 +70,108 @@ public class GameServer implements Runnable {
                 printMessage("I/O Error while communicating with Client.  Terminating connection.");
                 printMessage("    Message: " + e.getMessage());
             }
-            
+
             // Close the socket (and its streams)
             try {
                 printMessage("CLIENT is closing down.");
-                if (in != null) in.close();
-                if (out != null) out.close();
-                if (socket != null) socket.close();
+                if (in != null)
+                    in.close();
+                if (out != null)
+                    out.close();
+                if (socket != null)
+                    socket.close();
             } catch (IOException e) {
                 printMessage("Error trying to close socket. " + e.getMessage());
             }
         }
-        
+
         /**
          * Process one message that has been sent through this connection
+         * 
          * @param line The message to process
          **/
         private void processMessage(Object message) {
             // Process the line
-            //debug.println(3, "Processing message: " + message);
+            // debug.println(3, "Processing message: " + message);
 
-            if (message instanceof JoinMessage)
-            {
+            if (message instanceof JoinMessage) {
                 processJoinMessage((JoinMessage) message);
             }
             /*
-            else if (message instanceof MovePlayerMessage) {
-                processMovePlayerMessage((MovePlayerMessage) message);
-            }
-            */
-            else if (message instanceof ChangeDirectionMessage)
-            {
-            	processChangeDirectionMessage((ChangeDirectionMessage) message);
-            }
-            else
-            {
+             * else if (message instanceof MovePlayerMessage) {
+             * processMovePlayerMessage((MovePlayerMessage) message); }
+             */
+            else if (message instanceof ChangeDirectionMessage) {
+                processChangeDirectionMessage((ChangeDirectionMessage) message);
+            } else {
                 debug.println(3, "Unrecognized Message");
             }
         }
-        
+
         /*
-        private void processMovePlayerMessage(MovePlayerMessage message) {
-            if (this.playerID == -1) return;
-            gameEngine.setPlayerLocation(this.playerID, message.x, message.y);
-        }
-        */
-        
-        private void processChangeDirectionMessage(ChangeDirectionMessage message)
-        {
-        	 if (this.playerID == -1) return;
-        	 gameEngine.changePlayerDirection(this.playerID, message.newDirection);
+         * private void processMovePlayerMessage(MovePlayerMessage message) { if
+         * (this.playerID == -1) return; gameEngine.setPlayerLocation(this.playerID,
+         * message.x, message.y); }
+         */
+
+        private void processChangeDirectionMessage(ChangeDirectionMessage message) {
+            if (this.playerID == -1)
+                return;
+            gameEngine.changePlayerDirection(this.playerID, message.newDirection);
         }
 
-
-        private void processJoinMessage(JoinMessage message) 
-        {
+        private void processJoinMessage(JoinMessage message) {
             this.name = message.name;
             this.color = message.color;
             this.playerID = gameEngine.addPlayer(name, color);
-            debug.println(3, "Player "+ this.name + " is registered with id = " + this.playerID);
+            debug.println(3, "Player " + this.name + " is registered with id = " + this.playerID);
             transmitMessage(new JoinResponseMessage(this.name, this.playerID));
 
         }
 
-        public void transmitMessage(Object message)
-        {
-            try{
-                if (out == null) return;
-                synchronized(out)
-                {
+        public void transmitMessage(Object message) {
+            try {
+                if (out == null)
+                    return;
+                synchronized (out) {
                     out.writeObject(message);
-                    out.flush(); //ensures that info gets sent out immediately
+                    out.flush(); // ensures that info gets sent out immediately
                 }
-            }
-            catch (IOException|NullPointerException e)
-            {
+            } catch (IOException | NullPointerException e) {
                 debug.println(3, "Error transmitting message: " + message);
             }
 
-           // in = null;
-           // out = null;
-           // socket = null;
-                
-            }
-   
+            // in = null;
+            // out = null;
+            // socket = null;
+
+        }
 
         /**
          * Print out the message (with a little name id in front)
+         * 
          * @param message The message to print out
          **/
         private void printMessage(String message) {
-            System.out.println("["+ name + "]: " + message);
+            System.out.println("[" + name + "]: " + message);
         }
-    }  /* End Class Connection */
-    
+    } /* End Class Connection */
+
     int port;
     boolean done;
 
-    public GameServer(int port)
-    {
+    public GameServer(int port) {
         gameState = new GameState();
         gameEngine = new GameEngine();
-       
+
         debug = Debug.getInstance();
         connection = new HashSet<>();
         this.port = port;
     }
-    
+
     public GameServer() {
         this(DEFAULT_PORT);
     }
-
-
 
     /**
      * Run the main server... just listen for and create connections
@@ -191,7 +185,7 @@ public class GameServer implements Runnable {
             // Create a dedicated thread to transmit game state to all connections.
             createGameStateTransmitter();
 
-            //Start the game engine thread running
+            // Start the game engine thread running
             new Thread(gameEngine).start();
 
             while (!done) {
@@ -200,8 +194,7 @@ public class GameServer implements Runnable {
                 addConnection(clientSocket);
             }
         } catch (Exception e) {
-            System.err.println("ABORTING: An error occurred while creating server socket. " + 
-                               e.getMessage());
+            System.err.println("ABORTING: An error occurred while creating server socket. " + e.getMessage());
             System.exit(1);
         }
     }
@@ -211,13 +204,13 @@ public class GameServer implements Runnable {
         debug.println(3, "Game Server: Connecting to client: " + name);
         Connection c = new Connection(clientSocket, name);
         connection.add(c);
-        c.start();    // Start the thread.
+        c.start(); // Start the thread.
     }
-    /**
 
     /**
-     * Return a (deep) clone of the game state.
-     * Thus any changes to the game state must be made through the game server.
+     * 
+     * /** Return a (deep) clone of the game state. Thus any changes to the game
+     * state must be made through the game server.
      **/
     public GameState getGameState() {
         try {
@@ -233,43 +226,42 @@ public class GameServer implements Runnable {
     }
 
     private void createGameStateTransmitter() {
-        //we create an anonymous class for the thread
+        // we create an anonymous class for the thread
         Thread t = new Thread() {
             public void run() {
                 while (!done) {
-                    //debug.println(3, "Transmitting game states.");
-                    if (gameEngine != null)
-                    {	
-                    	//debug.println(3, "Transmitting game states.");
+                    // debug.println(3, "Transmitting game states.");
+                    if (gameEngine != null) {
+                        // debug.println(3, "Transmitting game states.");
                         GameState currentGameState = gameEngine.getGameState();
-                        //transmit game state to every connection
-                        //ONLY transmit if the player is registered
+                        // transmit game state to every connection
+                        // ONLY transmit if the player is registered
                         for (Connection con : connection) {
-                        	
-                            if (con.playerID != -1)
-                            {
+
+                            if (con.playerID != -1) {
                                 con.transmitMessage(currentGameState);
                             }
-                            
+
                         }
                     }
                     try {
                         Thread.sleep(GAME_STATE_REFRESH);
-                    } catch (Exception e) {}
+                    } catch (Exception e) {
+                    }
                 }
             }
         };
-        //must explicitly call the start method to start a thread
+        // must explicitly call the start method to start a thread
         t.start();
     }
 
     /**
-     * The main entry point.  It just processes the command line argument
-     * and starts an instance of the GameServer running.
+     * The main entry point. It just processes the command line argument and starts
+     * an instance of the GameServer running.
      **/
     public static void main(String[] args) {
         int port = DEFAULT_PORT;
-        
+
         // Set the port if specified
         if (args.length > 0) {
             try {
@@ -286,4 +278,3 @@ public class GameServer implements Runnable {
         s.run();
     }
 }
-
